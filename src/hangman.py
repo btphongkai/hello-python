c9727f7edfe4cee683e3006244d237073d075169
btphongkai <btphongkai@gmail.com>
2026-07-13 22:11:07 +0700

    temp

--- src/hangman.py
+++ src/hangman.py
@@ -1,1 +1,8 @@
-list_word = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew'];
+import random
+
+list_word = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew']
+word = random.choice(list_word)
+word_to_guess = ['_'] * len(word)
+print("Welcome to Hangman Game!");
+print("The word to guess has", len(word), "letters.")
+print(" ".join(word_to_guess))
